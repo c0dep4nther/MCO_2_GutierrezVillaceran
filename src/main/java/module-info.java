88c782ef3dc22009c6a3942ecaa341747444b0ed
Mark Daniel Gutierrez <markdcg@outlook.com>
2022-11-27 18:27:35 +0800

    Initial GUI Build for MyFarm

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.myfarm.mco_2_gutierrezvillaceran {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.myfarm.mco_2_gutierrezvillaceran to javafx.fxml;
+    exports com.myfarm.mco_2_gutierrezvillaceran;
+}
