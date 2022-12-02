f844ea4a6e4512e86a48d56a6f6ad7d239db46a5
Mark Daniel Gutierrez <markdcg@outlook.com>
2022-12-02 20:52:30 +0800

    Added Grid Buttons
    - listeners are subject to implementation

--- src/main/java/com/myfarm/mco_2_gutierrezvillaceran/GameController.java
+++ src/main/java/com/myfarm/mco_2_gutierrezvillaceran/GameController.java
@@ -3,10 +3,13 @@ package com.myfarm.mco_2_gutierrezvillaceran;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,11 +26,11 @@ public class GameController implements Initializable {
     @FXML
     private Button myButton;
     @FXML
-    private Button plant;
+    private Button plantBtn;
     @FXML
-    private Button harvest;
+    private Button harvestBtn;
     @FXML
-    private Button water;
+    private Button waterBtn;
     @FXML
     private Button fertilize;
     @FXML
@@ -35,9 +38,12 @@ public class GameController implements Initializable {
     @FXML
     private Button pickaxe;
     @FXML
-    private Button plow;
+    private Button plowBtn;
+    @FXML
+    private Button tileBtn;
     @FXML
     private GridPane farmGrid;
+    private Stage stage;
 
 
     String choice;
@@ -61,7 +67,44 @@ public class GameController implements Initializable {
         currentDay = gameModel.getDayCount();
         currentMoney = gameModel.getMoney();
 
+        // center report label
+//        report.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
         report.setText("Day: " + currentDay + "\nMoney: " + currentMoney);
+
+        addButtons();
+    }
+
+    public void onPlowBtnClick(ActionEvent actionEvent) {
+//        gameModel.plow();
+//        report.setText("Day: " + currentDay + "\nMoney: " + currentMoney);
+    }
+
+    // loop through the grid and add button
+    public void addButtons() {
+        int count = 1;
+        int row;
+        int column;
+
+        for (column = 0; column < 5; column++) {
+            for (row = 0; row < 10; row++) {
+                tileBtn = new Button();
+                tileBtn.setText("Tile " + count);
+                farmGrid.add(tileBtn, column, row);
+                count+=5;
+
+                // center align grid elements
+                GridPane.setHalignment(tileBtn, HPos.CENTER);
+                GridPane.setValignment(tileBtn, VPos.CENTER);
+            }
+
+            count = switch (count) {
+                case 51 -> 2;
+                case 52 -> 3;
+                case 53 -> 4;
+                case 54 -> 5;
+                default -> 0;
+            };
+        }
     }
 
 
@@ -77,16 +120,14 @@ public class GameController implements Initializable {
     }
 
     public void evaluateChoice(String choice){
-        switch (choice) {
-            case "P" -> {
-                report.setText("What would you like to plant? \n[1] Turnip\n[2] Carrot");
+        if ("P".equals(choice)) {
+            report.setText("What would you like to plant? \n[1] Turnip\n[2] Carrot");
 
-                //seedChoice = input.nextInt();
-                //input.nextLine();
+            //seedChoice = input.nextInt();
+            //input.nextLine();
 
-                //System.out.println("Where would you like to plant it?");
-                //farmLand = player.plantSeed(seedList.get(seedChoice), farmLand);
-            }
+            //System.out.println("Where would you like to plant it?");
+            //farmLand = player.plantSeed(seedList.get(seedChoice), farmLand);
         }
     }
 }
